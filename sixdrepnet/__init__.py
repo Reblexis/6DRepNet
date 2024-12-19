930f838dafe72b175d371ea5812423c415ab69f9
Viktor Cihal <viktor.cihal@gmail.com>
2024-12-19 11:55:17 +0100

    Update imports

--- sixdrepnet/__init__.py
+++ sixdrepnet/__init__.py
@@ -1,5 +1,9 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from sixdrepnet.regressor import SixDRepNet_Detector as SixDRepNet
+from sixdrepnet import backbone
+from sixdrepnet import loss
+from sixdrepnet import model
+from sixdrepnet import utils
 
 
 """
